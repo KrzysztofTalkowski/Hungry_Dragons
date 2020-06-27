226de38ed5ce15eae02a03267c0822ab543074bb
Krzysztof Talkowski <k.talkowski@gmail.com>
2020-06-27 22:12:38 +0200

    Added Enemies + Shoot logic

--- Hungry Birds/Assets/Dragon.cs
+++ Hungry Birds/Assets/Dragon.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Dragon : MonoBehaviour
 {
     private Vector3 _initialPosition;
-    private bool _birdWasLaunched;
+    private bool _dragonWasLaunched;
     private float _timeSittingAround;
 
 
@@ -20,17 +20,20 @@ public class Dragon : MonoBehaviour
 
     private void Update()
     {
-        if (_birdWasLaunched &&
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+        
+        if (_dragonWasLaunched &&
             GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
         {
             _timeSittingAround += Time.deltaTime;
         }
 
 
-        if (transform.position.y > 10 ||
-            transform.position.y < -10 ||
-            transform.position.x > 10 ||
-            transform.position.x < -20 ||
+        if (transform.position.y > 7 ||
+            transform.position.y < -15 ||
+            transform.position.x > 15 ||
+            transform.position.x < -25 ||
             _timeSittingAround > 2.5)
         {
             string currentSceneName = SceneManager.GetActiveScene().name;
@@ -41,6 +44,7 @@ public class Dragon : MonoBehaviour
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.yellow;
+        GetComponent<LineRenderer>().enabled = true;
     }
 
     private void OnMouseUp()
@@ -48,10 +52,12 @@ public class Dragon : MonoBehaviour
         GetComponent<SpriteRenderer>().color = Color.white;
 
         Vector2 directionToInitialPosition = _initialPosition - transform.position;
-        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
 
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
-        _birdWasLaunched = true;
+        _dragonWasLaunched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
     }
 
     private void OnMouseDrag()
