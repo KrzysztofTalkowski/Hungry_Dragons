4f94da074e8ebeb00366847f2f5ad682f9aa1b45
Krzysztof Talkowski <k.talkowski@gmail.com>
2020-07-02 04:22:57 +0200

    Working with EndLevelScreen and UIElements

---
+++ Hungry Birds/Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+    private static int _nextLevelIndex = 1;
+    public GameObject EndScreen;
+    public float seconds = 5f;
+
+    IEnumerator LateCall()
+    {
+
+        yield return new WaitForSeconds(seconds);
+
+        EndScreen.gameObject.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    void Update()
+    {
+        
+
+
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("You killed all enemies");
+        StartCoroutine(LateCall());
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+
+        SceneManager.LoadScene(nextLevelName);
+
+    }
+}
---
+++ Hungry Birds/Assets/Scripts/LevelTextUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelTextUI : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("Active Scene is '" + scene.name + "'.");
+    }
+}
